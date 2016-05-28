bfd97aa5413eb864377bb4853d2b00502090043f
kenleyt <kenleyt99@gmail.com>
2016-05-28 19:17:52 +0000

    add twitter keys

---
+++ helpers.js
@@ -0,0 +1,24 @@
+// Helpers
+// Global dependencies 
+
+var Twitter = require("twitter");
+
+var helpers = {};
+
+Helpers.getTwitterClient = function(){
+
+// TODO: this is super insercure.
+// move to envronment variables.
+
+    var client = new Twitter({
+      consumer_key: '6hIro2boWDeFMV3P1BRRH0SC8',
+      consumer_secret: 'ASDXB4loBSLGxJ691e0aMoTPbc6BlHpWySrTfYkoOjCE8X4rAS',
+      access_token_key: '3917297299-OUiifc5oQ4xEAhksrG4dI6I3oHDPUq7eGUalGCP',
+      access_token_secret: '5REE4OgCt9PIJLfnLbH9DZcSqEOqbgcEHqExhMEtpC7Y8'
+        });
+
+
+    return client;
+};
+
+module.exports = Helpers
